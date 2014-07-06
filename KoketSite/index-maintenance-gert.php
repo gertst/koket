4dbaad8fb8613d1e845003c7f1f26f1ebbd6a56e
Gert Stalpaert <gertst@gmail.com>
2014-07-06 22:16:29 +0200

    full site download - cont

---
+++ KoketSite/index-maintenance-gert.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+        "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+    <title>Kokette Katinka</title>
+    <meta name="description" content="Kokette Katinka verkoopt stoffen, kralen en juwelen.
+Je vind Kokette Katinka zowel online als via de winkel in Antwerpen centrum." />
+    <meta name="keywords" content="Kokette Katinka, stoffen, kralen, juwelen" />
+    <link rel="icon" href="http://www.kokettekatinka.be/media/favicon/default/icon_Kokette-Katinka_2.png" type="image/x-icon" />
+    <link rel="shortcut icon" href="http://www.kokettekatinka.be/media/favicon/default/icon_Kokette-Katinka_2.png" type="image/x-icon" />
+    <link href='http://fonts.googleapis.com/css?family=Oswald|Alef' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" media="screen,print" href="/maintenance.css" />
+    <meta name="description" content="Wij verkopen kokette kralen, stoffen en juwelen.  Binnenkort kan je hier terecht op onze web shop. Ook onze winkel in Antwerpen centrum gaat weldra open."/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+
+</head>
+<body>
+
+<div class="frameT">
+    <div class="frameTC">
+
+        <div class="content">
+
+            <img class="logo" src="http://www.kokettekatinka.be/media/koket/Logo-Kokette-Katinka-100x100.png" alt="logo">
+            <h1>Kokette Katinka</h1>
+
+            <p><strong>Wij verkopen hippe stoffen, zalige kralen en vrolijke juwelen.</strong></p>
+            <p>Binnenkort kun je hier terecht op onze webshop.<br>
+            Ook onze winkel in Antwerpen centrum gaat weldra open.</p>
+            <p>Wil je op de hoogte blijven? Like <a href="http://www.facebook.com/kokettekatinka">onze facebookpagina</a><!--en/of schrijf je in voor de nieuwsbrief-->.</p>
+
+        </div>
+
+    </div>
+</div>
+
+</body>
+</html>
