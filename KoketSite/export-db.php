50806d28e3441c8bbfb661a1e8c03cc0a02bf5a2
Gert Stalpaert <gertst@gmail.com>
2014-11-23 21:15:56 +0100

    export db script Facebook tab: whats new

--- KoketSite/export-db.php
+++ KoketSite/export-db.php
@@ -6,3 +6,5 @@
  * Time: 14:50
  * To change this template use File | Settings | File Templates.
  */
+$command = "mysqldump -ukokettek_main -pVytOeHOWr6hH5Hu --default-character-set=utf8 kokettek_koket > backup.sql";
+exec($command, $output, $return_var);
--- KoketSite/facebook.php
+++ KoketSite/facebook.php
@@ -23,6 +23,35 @@ $headBlock->addCss('css/styles.css');
 $headBlock->getCssJsHtml();
 $headBlock->getIncludes();
 
+$_helper    = Mage::helper('catalog/output');
+
+
+function isNew($product)
+{
+    if ($product->getData('featured_product')) {
+        return true;
+    }
+
+    if ($product->getData('news_from_date') == null && $product->getData('news_to_date') == null) {
+        return false;
+    }
+
+    if ($product->getData('news_from_date') !== null) {
+        if (date('Y-m-d', strtotime($product->getData('news_from_date'))) > date('Y-m-d', time())) {
+            return false;
+        }
+    }
+
+    if ($product->getData('news_to_date') !== null) {
+        if (date('Y-m-d', strtotime($product->getData('news_to_date'))) < date('Y-m-d', time())) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
@@ -30,28 +59,45 @@ $headBlock->getIncludes();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title>Kokette Katinka Online Webshop</title>
-    <link type="text/css" rel="stylesheet" href="https://www.kokettekatinka.be/skin/m/1414439524/skin/frontend/shopper/default/css/styles.css,/skin/frontend/base/default/css/widgets.css,/skin/frontend/base/default/css/magestore/rewardpoints.css,/skin/frontend/base/default/css/rewardpointsbehavior/behavior.css,/skin/frontend/base/default/css/rewardpointsreferfriends/referfriends.css,/skin/frontend/shopper/default/css/cloud-zoom.css,/skin/frontend/shopper/default/js/fancybox/jquery.fancybox-1.3.4.css,/skin/frontend/shopper/default/css/slider.css,/skin/frontend/shopper/default/css/local.css,/skin/frontend/shopper/default/css/responsive.css,/skin/frontend/shopper/default/css/mobile.css,/skin/frontend/shopper/default/css/animation.css,/skin/frontend/shopper/default/css/settings.css,/skin/frontend/shopper/default/css/captions.css,/skin/frontend/shopper/koket/css/override.css" media="all" />
+    <title>Kokette Katinka - Kralen, Stoffen en Juwelen</title>
+    <link type="text/css" rel="stylesheet" href="http://www.kokettekatinka.be/skin/m/1414439524/skin/frontend/shopper/default/css/styles.css,/skin/frontend/base/default/css/widgets.css,/skin/frontend/base/default/css/magestore/rewardpoints.css,/skin/frontend/base/default/css/rewardpointsbehavior/behavior.css,/skin/frontend/base/default/css/rewardpointsreferfriends/referfriends.css,/skin/frontend/shopper/default/css/cloud-zoom.css,/skin/frontend/shopper/default/js/fancybox/jquery.fancybox-1.3.4.css,/skin/frontend/shopper/default/css/slider.css,/skin/frontend/shopper/default/css/local.css,/skin/frontend/shopper/default/css/responsive.css,/skin/frontend/shopper/default/css/mobile.css,/skin/frontend/shopper/default/css/animation.css,/skin/frontend/shopper/default/css/settings.css,/skin/frontend/shopper/default/css/captions.css,/skin/frontend/shopper/koket/css/override.css" media="all" />
+    <link href='https://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:300,400,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Alegreya:300,400,700' rel='stylesheet' type='text/css'>
 </head>
 <body>
+
+<div class="page-title-bg" style="position: inherit;background-color: #eefaed">
+    <h1 style="padding: 30px 0;font-family: 'Yanone Kaffeesatz',Tahoma,sans-serif;text-transform: uppercase">Een greep uit onze nieuwste producten in <a href="http://www.kokettekatinka.be" target="koket">de webshop</a></h1>
+</div>
 
 <div class="category-products">
 <ol class="products-list" id="products-list" style="padding: 20px 20px;">
 <?php
 
-$category = Mage::getModel('catalog/category')->load(31); //nice price stoffen
-$category = Mage::getModel('catalog/category')->load(32); //nice price kralen
-$collection = $category->getProductCollection()->addAttributeToSort('position');
+$collection = Mage::getModel('catalog/category')->load(2)
+    ->getProductCollection()
+    ->addAttributeToSelect('*')
+    ->addFieldToFilter('visibility', array(
+        Mage_Catalog_Model_Product_Visibility::VISIBILITY_BOTH,
+        Mage_Catalog_Model_Product_Visibility::VISIBILITY_IN_CATALOG
+    )) //showing just products visible in catalog or both search and catalog
+    ->addAttributeToSort('created_at', 'DESC')
+    ->setPageSize(150) // limit number of results returned
+    ->setCurPage(0);
 
-//$catcount = $collection->count();
-Mage::getModel('catalog/layer')->prepareProductCollection($collection);
 foreach ($collection as $product) {
+    if (!strpos($product->getSku(), "sample") && isNew($product)) {
     ?>
     <li class="item odd">
         <div class="f-left">
-            <a href="<?php echo $product->getProductUrl(); ?>" target="_blank"
+            <a href="<?php echo $product->getProductUrl(); ?>" target="koket"
                title="<?php echo $product->getName(); ?>" class="product-image">
-                <div class="sale-label sale-top-right"></div>
+                <?php if ($product->getSpecialPrice() > 0) { ?>
+                    <div class="sale-label sale-top-right"></div>
+                <?php } ?>
+                <?php  if (isNew($product)) { ?>
+                    <div class="new-label new-top-left"></div>
+                <?php } ?>
                 <img
                     src="<?php echo Mage::helper('catalog/image')->init($product, 'thumbnail')->resize(252); ?>"
                     width="252" height="252" alt="<?php echo $product->getName(); ?>">
@@ -59,23 +105,29 @@ foreach ($collection as $product) {
         </div>
         <div class="product-shop">
             <div class="f-fix">
-                <h2 class="product-name"><a href="<?php echo $product->getProductUrl(); ?>" target="_blank"
+                <h2 class="product-name"><a href="<?php echo $product->getProductUrl(); ?>" target="koket"  style="font-family: 'Yanone Kaffeesatz',Tahoma,sans-serif; font-size: 18px;"
                                             title="<?php echo $product->getName(); ?>"><?php echo $product->getName(); ?></a>
                 </h2>
 
                 <div class="price-box">
-                    <p class="old-price">
-                            <span class="price">
-                                <?php
-                                $price = Mage::helper('core')->currency($product->getPrice(), true, false);
-                                echo $price; ?>
-                            </span>
-                    </p>
-
+                    <?php if ($product->getSpecialPrice() > 0) { ?>
+                        <p class="old-price">
+                                <span class="price" style="font-family: Alegreya,Tahoma,sans-serif;">
+                                    <?php
+                                    $price = Mage::helper('core')->currency($product->getPrice(), true, false);
+                                    echo $price; ?>
+                                </span>
+                        </p>
+                    <?php } ?>
                     <p class="regular-price">
-                        <span class="price">
+                        <span class="price"  style="font-family: Alegreya,Tahoma,sans-serif;">
                             <?php
-                            $price = Mage::helper('core')->currency($product->getSpecialPrice(), true, false);
+                            if ($product->getSpecialPrice() > 0) {
+                                $price = $product->getSpecialPrice();
+                            } else {
+                                $price = $product->getPrice();
+                            }
+                            $price = Mage::helper('core')->currency($price, true, false);
                             echo $price; ?>
                         </span>
 
@@ -85,21 +137,25 @@ foreach ($collection as $product) {
                 </div>
 
                 <div class="desc std">
-                    <?php echo $product->getShortDescription() ?>
-
+                    <?php
+                        echo $_helper->productAttribute($product, nl2br($product->getShortDescription()), 'short_description') ?>
                 </div>
-                <p><a href="<?php echo $product->getProductUrl(); ?>" target="_blank">Meer info</a></p>
+                <p><a href="<?php echo $product->getProductUrl(); ?>" target="koket">Meer info</a></p>
 
             </div>
         </div>
     </li>
 
 <?php
+    }//if
 }
 
 
 ?>
 </ol>
 </div>
+
+<p style="margin-bottom: 40px;">Je vindt nog meer nieuwe producten op onze site <a href="http://www.kokettekatinka.be" target="koket">www.kokettekatinka.be</a></p>
+
 </body>
 </html>
